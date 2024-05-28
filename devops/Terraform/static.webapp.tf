77eccdf65d5dcf01346864e450024ed0e9665add
Abdul Jameel <b.abduljameel@gmail.com>
2024-05-28 21:28:56 +0530

    included static web app for react service

---
+++ devops/Terraform/static.webapp.tf
@@ -0,0 +1,7 @@
+resource "azurerm_static_web_app" "cloudresumechallange_staicsite" {
+  name                = "${var.appname}${var.env}staticsite"
+  location            = "eastasia"
+  resource_group_name = azurerm_resource_group.cloudresumechallenge_resourcegroup.name
+  sku_tier            = "Free"
+  sku_size            = "Free"
+}
