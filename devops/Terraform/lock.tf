5f6151a1213d3433d54a5e4012258bb0507769f9
Abdul Jameel <b.abduljameel@gmail.com>
2024-06-01 18:47:56 +0530

    removed lock

--- devops/Terraform/lock.tf
+++ devops/Terraform/lock.tf
@@ -1,5 +1,5 @@
-resource "azurerm_management_lock" "azurerm_management_lock" {
-  name       = "lock"
-  scope      = azurerm_storage_account.cloudresumechallenge_storageaccount.id
-  lock_level = "CanNotDelete"
-}
+# resource "azurerm_management_lock" "azurerm_management_lock" {
+#   name       = "lock"
+#   scope      = azurerm_storage_account.cloudresumechallenge_storageaccount.id
+#   lock_level = "CanNotDelete"
+# }
