3de248202a34736464780daf8823be8d1fc7cbcc
Abdul Jameel <b.abduljameel@gmail.com>
2024-06-02 09:46:56 +0530

    included header function key

--- frontend/Cloud-Resume-UI/src/services/AxoisInstance.ts
+++ frontend/Cloud-Resume-UI/src/services/AxoisInstance.ts
@@ -8,6 +8,10 @@ import axios from "axios";
 
 const axoisInstance = axios.create({
   baseURL: "https://cloudresumechallengedevfunctionapp.azurewebsites.net/api",
+  headers: {
+    "x-functions-key":
+      "7k3fEs6a2EwBwOmwwII10Yp9n7bsd129BMkUMJFeKvkNAzFuG8KNLw==",
+  },
 });
 
 export default axoisInstance;
