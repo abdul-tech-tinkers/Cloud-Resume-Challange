d45b3d5076484729f2a72e4cbdef287a3ee2a822
Abdul Jameel <b.abduljameel@gmail.com>
2024-06-03 16:33:34 +0530

    temp route fix

--- frontend/Cloud-Resume-UI/src/screens/HomeScreen.tsx
+++ frontend/Cloud-Resume-UI/src/screens/HomeScreen.tsx
@@ -2,13 +2,14 @@ import { Container, Flex, Separator } from "@radix-ui/themes";
 import FooterScreen from "./FooterScreen";
 import HeaderScreen from "./HeaderScreen";
 import { Outlet } from "react-router-dom";
+import DashboardScreen from "./DashboardScreen";
 
 const HomeScreen = () => {
   return (
     <Container size="3" align="center" style={{ height: "100vh" }}>
       <HeaderScreen />
       <Flex style={{ background: "transparent", flex: 1 }}>
-        <Outlet />
+        <DashboardScreen />
       </Flex>
       <Separator orientation="horizontal" size="4" my="4" />
       <FooterScreen />
