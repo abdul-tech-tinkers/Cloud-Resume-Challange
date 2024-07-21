a8fe22023d9860f2f626aeb03380a10b57d4f917
Abdul Jameel <b.abduljameel@gmail.com>
2024-07-21 18:27:27 +0530

    FIX

--- frontend/Cloud-Resume-UI/src/components/AppProjectItem.tsx
+++ frontend/Cloud-Resume-UI/src/components/AppProjectItem.tsx
@@ -1,5 +1,5 @@
 import { Project } from "../models/Project";
-import { Badge, Flex, Heading, Text } from "@radix-ui/themes";
+import { Badge, Flex, Separator, Text } from "@radix-ui/themes";
 import AppProjectDate from "./AppProjectDate";
 import AppProjectLinkItem from "./AppProjectLinkItem";
 import AppLink from "./AppLink";
@@ -11,31 +11,31 @@ interface props {
 
 const AppProjectItem = ({ project }: props) => {
   return (
-    <Flex my="3">
-      <Flex direction="column" gap="2">
-        <Flex align="center" gapY="2" gapX="5" justify="start">
-          <AppProjectLinkItem project={project} />
-          <Badge color="blue">
-            {project.Type === 1 ? "Work Project" : "Personal Project"}
-          </Badge>
-          <AppLink href={project.GithubLink}>
-            <GrGithub />
-          </AppLink>
-        </Flex>
-        <AppProjectDate
-          startDate={project.StartDate}
-          endDate={project.EndDate}
-        />
-        <Text>{project.Description}</Text>
+      <Flex my="3">
+        <Flex direction="column" gap="2">
+          <Flex align="center" gapY="2" gapX="5" justify="start">
+            <AppProjectLinkItem project={project} />
+            <Badge color="blue">
+              {project.Type === 1 ? "Work Project" : "Personal Project"}
+            </Badge>
+            <AppLink href={project.GithubLink}>
+              <GrGithub />
+            </AppLink>
+          </Flex>
+          <AppProjectDate
+              startDate={project.StartDate}
+              endDate={project.EndDate}
+          />
+          <Text>{project.Description}</Text>
 
-        <Flex direction="row" gap="2">
-          {project?.TechStacks?.map((tag) => (
-            <Badge key={tag}>{tag}</Badge>
-          ))}
+          <Flex direction="row" gap="2">
+            {project?.TechStacks?.map((tag) => (
+                <Badge key={tag}>{tag}</Badge>
+            ))}
+          </Flex>
+          <Separator orientation="horizontal" size="3" />
         </Flex>
-        <Separator orientation="horizontal" size="3" />
       </Flex>
-    </Flex>
   );
 };
 
